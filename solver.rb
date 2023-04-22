ad092f1d19a5e91ea139e94e4d854d829f345838
mrprotocoll <lekanvgbg@gmail.com>
2023-04-22 07:57:29 +0100

    Added factorial method to solver class

--- solver.rb
+++ solver.rb
@@ -1,3 +1,15 @@
 class Solver
-    
+  def factorial(value)
+    if value < 0
+      raise ArgumentError, "Factorial of a negative integer is undefined"
+    elsif value == 0
+      return 1
+    else
+      result = 1
+      (1..value).each do |i|
+        result *= i
+      end
+      return result
+    end
+  end
 end
