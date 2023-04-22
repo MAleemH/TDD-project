2f5c3cb8a246786cc035665c631151f1113fd97a
mrprotocoll <lekanvgbg@gmail.com>
2023-04-22 08:01:06 +0100

    Fixed linters error

--- solver.rb
+++ solver.rb
@@ -1,31 +1,31 @@
 class Solver
   def factorial(value)
-    if value < 0
-      raise ArgumentError, "Factorial of a negative integer is undefined"
-    elsif value == 0
-      return 1
+    if value.negative?
+      raise ArgumentError, 'Factorial of a negative integer is undefined'
+    elsif value.zero?
+      1
     else
       result = 1
       (1..value).each do |i|
         result *= i
       end
-      return result
+      result
     end
   end
 
   def reverse(value)
-    return value.reverse
+    value.reverse
   end
 
   def fizzbuzz(value)
-    if value % 3 == 0 && value % 5 == 0
-        return "fizzbuzz"
-      elsif value % 3 == 0
-        return "fizz"
-      elsif value % 5 == 0
-        return "buzz"
-      else
-        return value.to_s
+    if (value % 3).zero? && (value % 5).zero?
+      'fizzbuzz'
+    elsif (value % 3).zero?
+      'fizz'
+    elsif (value % 5).zero?
+      'buzz'
+    else
+      value.to_s
     end
   end
 end
